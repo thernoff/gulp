4bb4fd1882dcd0ebbf19d6c349e2eaf8a9b1528b
thernoff <thernoff@yandex.ru>
2019-02-19 06:13:32 +0300

    Уроки 7, 8, 9: Включение компиляции Less в основной gulp таск. Обработка ошибок. Добавление кроссбраузерных префиксов.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 // Подключаем пакеты
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var autoprefixer = require('gulp-autoprefixer');
 
-// Создаем задачи
+// ********** Создаем тестовые задачи **********
 gulp.task('task-before-1', function () {
   return new Promise(function (resolve, reject) {
     console.log('Task before 1');
@@ -27,31 +32,63 @@ gulp.task('start-2', gulp.series(['task-before-1', 'task-before-2'], function (d
   done();
 }))
 
-// Урок 4: Обновление проекта с browsersync
-var browserSync = require('browser-sync').create();
-gulp.task('server', function (done) {
+
+// ********** Создаем задачи **********
+
+// Task для компиляции Less (1 способ)
+
+/* gulp.task('less', function() {
+  return new Promise(function (resolve, reject) {
+    return gulp.src('./src/less/main.less')
+      .pipe(less())
+      .pipe(gulp.dest('./src/css/'))
+      // метод stream точечно обновляет только одни стили, а reload обновляет всю страницу
+      .pipe(browserSync.stream())
+      .pipe(resolve());
+  });
+}); */
+
+// Task для компиляции Less (2 способ)
+gulp.task('less', function(done) {
+    return gulp.src('./src/less/main.less')
+      // Обработка ошибок: перед тем как начать обработку, сначала запустим plumber
+      .pipe(
+        plumber({
+          errorHandler: notify.onError( (err) => {
+            return {
+              title: 'Styles', // указываем свой title
+              message: err.message
+            }
+          } )
+        })
+      )
+      // Преобразование less в css
+      .pipe(less())
+      // Расстановка автопрефиксов
+      .pipe(
+        autoprefixer({
+          browsers: ['last 6 versions'], // указываем количество последних браузеров, для которых нужно раставлять автопрефиксы
+          cascade: false
+        })
+      )      
+      .pipe(gulp.dest('./src/css/'))
+      // метод stream точечно обновляет только одни стили, а reload обновляет всю страницу
+      .pipe(browserSync.stream())
+      .on('end', done);
+
+});
+
+// Создаем задачу
+gulp.task('server', gulp.series(['less'], function (done) {
   browserSync.init({
     server: { baseDir: './src/' }
   });
 
   gulp.watch('src/**/*.html').on('change', browserSync.reload);
-  gulp.watch('src/css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('src/less/**/*.less', gulp.series(['less']));
   gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
   done();
-});
+}));
 
+// Создаем задачу по умолчанию
 gulp.task('default', gulp.series(['server']));
-
-// Урок 6: Task для компиляции Less
-var less = require('gulp-less');
-
-gulp.task('less', function() {
-  return new Promise(function (resolve, reject) {
-    return gulp.src('./src/less/main.less')
-      .pipe(less())
-      .pipe(gulp.dest('./src/css/'))
-      .on('end', () => {
-        resolve();
-      });
-  });
-});
